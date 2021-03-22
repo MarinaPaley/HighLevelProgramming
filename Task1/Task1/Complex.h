397eff529787e58722a719d14b123ab8de2d901d
MarinaPaley <marina_paley@mail.ru>
2021-03-22 15:29:21 +0300

    Исправлен API класса #1
    1. Убраны пустые тела функций (ошибочные)

--- Task1/Task1/Complex.h
+++ Task1/Task1/Complex.h
@@ -17,12 +17,12 @@ public:
      * \param re Действительная часть
      * \param im Мнимая часть
      */
-    Complex(const double re, const double im) {};
+    Complex(const double re, const double im);
     /**
      * \brief Конструктор копирования
      * \param other Комплексное число
      */
-    Complex(const Complex& other) {};
+    Complex(const Complex& other);
     /**
      * \brief Деструктор
      */
@@ -32,58 +32,58 @@ public:
      * \brief Метод, возвращающий действительную часть
      * \return Действительная часть комплексного числа
      */
-    double GetRe() const {};
+    double GetRe() const;
     /**
      * \brief Метод, возвращающий мнимую часть
      * \return Мнимая часть комплексного числа
      */
-    double GetIm() const {};
+    double GetIm() const;
     /**
      * \brief Метод, возвращающий модуль комплексного числа
      * \return Модуль комплексного числа
      */
-    double GetModulus() const {};
+    double GetModulus() const;
     /**
      * \brief Метод, возвращающий аргумент
      * \return Аргумент комплексного числа
      */
-    double GetArgument() const {};
+    double GetArgument() const;
     /**
      * \brief Метод, возвращающий комплексно сопряженное число
      * \return Комплексно сопряженное число
      */
-    Complex& GetConjugate() const {};
+    Complex& GetConjugate() const;
 
     /**
      * \brief Алгебраическое представление комплексного числа
      * \return Строковое представление комплексного числа
      * в алгебраическом виде
      */
-    std::string GetAlgebraView() const {};
+    std::string GetAlgebraView() const;
     /**
      * \brief Тригонометрическое представление комплексного числа
      * \return Строковое представление комплексного числа
      * в тригонометрическом виде
      */
-    std::string GetTrigonometryView() const {};
+    std::string GetTrigonometryView() const;
     /**
      * \brief Экспоненциальное представление комплексного числа
      * \return Строковое представление комплексного числа
      * в экспоненциальном виде
      */
-    std::string GetExponentialView() const {};
+    std::string GetExponentialView() const;
 
     /**
      * \brief Метод сложения с другим комплексным числом
      * \param other Другое комплексное число
      * \return Третье (результат) комплексное число
      */
-    Complex& Add(const Complex& other) const {};
-    Complex& Sub(const Complex& other) const {};
-    Complex& Mul(const Complex& other) const {};
-    Complex& Div(const Complex& other) const {};
+    Complex& Add(const Complex& other) const;
+    Complex& Sub(const Complex& other) const;
+    Complex& Mul(const Complex& other) const;
+    Complex& Div(const Complex& other) const;
 
-    bool AreEqual(const Complex& other) const {};
+    bool AreEqual(const Complex& other) const;
 
 private:
     double re;
